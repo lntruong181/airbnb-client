22ab2c98ba21d2a5ac66e323da8766f331fb3760
lntruong181 <lntruong181@gmail.com>
2022-11-27 15:15:23 +0700

    implement lodash

--- src/features/Home/Album/AlbumItem/AlbumItem.jsx
+++ src/features/Home/Album/AlbumItem/AlbumItem.jsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames/bind';
-import axios from 'axios';
-
+import _ from 'lodash';
 import Image from '@/components/Image';
 import { StarIcon } from '@/enum/icon.enum';
 
@@ -13,67 +12,27 @@ import Carousel from '@/components/Carousel';
 const cx = classNames.bind(styles);
 
 const AlbumItem = () => {
-  // const randomImage = async () => {
-  //   await axios.get('https://api.unsplash.com//photos/random');
-  // };
+  const imageCollections = [
+    'https://images.unsplash.com/photo-1528127269322-539801943592?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dmlldG5hbSUyMHBvcHVsYXIlMjBsb2NhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1504457047772-27faf1c00561?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlldG5hbSUyMHBvcHVsYXIlMjBsb2NhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1464809142576-df63ca4ed7f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmlldG5hbSUyMHBvcHVsYXIlMjBsb2NhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1531737212413-667205e1cda7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dmlldG5hbSUyMHBvcHVsYXIlMjBsb2NhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1552374516-0fffe5103f4c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHZpZXRuYW0lMjBwb3B1bGFyJTIwbG9jYXRpb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  ];
+
   return (
     <div className={cx('container')}>
       <div className={cx('item')}>
         <div className={cx('carousel')}>
           <Carousel>
-            <Image
-              src={
-                'https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-              }
-              alt='places'
-              className={cx('image')}
-            />
-            <Image
-              src={
-                'https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-              }
-              alt='places'
-              className={cx('image')}
-            />
-            <Image
-              src={
-                'https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-              }
-              alt='places'
-              className={cx('image')}
-            />
-            <Image
-              src={
-                'https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-              }
-              alt='places'
-              className={cx('image')}
-            />
-            <Image
-              src={
-                'https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-              }
-              alt='places'
-              className={cx('image')}
-            />
-            <Image
-              src={
-                'https://images.unsplash.com/photo-1520962922320-2038eebab146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-              }
-              alt='places'
-              className={cx('image')}
-            />
+            {_.shuffle(imageCollections).map((image) => (
+              <Image src={image} alt='beauty-spot' className={cx('image')} />
+            ))}
           </Carousel>
         </div>
         <span className={cx('like')}>
           <Image src={images.heartIcon} />
         </span>
-        {/* <Button className={cx('next')} isRound={true} scaleOnHover={true}>
-          <RightArrowIcon />
-        </Button>
-        <Button className={cx('previous')} isRound={true} scaleOnHover={true}>
-          <LeftArrowIcon />
-        </Button> */}
       </div>
       <div className={cx('item')}>
         <div className={cx('location')}>
